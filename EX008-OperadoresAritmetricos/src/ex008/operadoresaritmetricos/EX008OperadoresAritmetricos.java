86a142ff5b8a9f3738024f90a6abda99cd71ab08
RodrigoMSCruz <85855194+RodrigoMSCruz@users.noreply.github.com>
2021-12-14 11:38:44 -0300

    Add files via upload

---
+++ EX008-OperadoresAritmetricos/src/ex008/operadoresaritmetricos/EX008OperadoresAritmetricos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex008.operadoresaritmetricos;
+
+/**
+ *
+ * @author rodrigomscruz
+ */
+public class EX008OperadoresAritmetricos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1, n2;
+        float m;
+        n1 = 3;
+        n2 = 5;
+        m = (n1 + n2) / 2;
+        System.out.println("A média é igual a: " + m);
+    }
+    
+}
