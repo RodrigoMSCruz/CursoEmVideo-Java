eb104f18532f5418ffc2d7249245de99b3dfbeb9
RodrigoMSCruz <85855194+RodrigoMSCruz@users.noreply.github.com>
2021-12-15 15:48:53 -0300

    Add files via upload

---
+++ EX011-OperadorTernario/src/ex011/operadorternario/EX011OperadorTernario.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex011.operadorternario;
+
+/**
+ *
+ * @author rodrigomscruz
+ */
+public class EX011OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1, n2, r;
+        n1 =4;
+        n2 = 8;
+        r = (n1>n2)?0:1;
+        System.out.println(r);
+    }
+    
+}
